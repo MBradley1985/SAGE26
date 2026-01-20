7ec3917965dc44199a05cdd62f253d36a8334710
MBradley1985 <2496844@student.swin.edu.au>
2026-01-20 11:05:47 +1100

    -

---
+++ run_sage_flythrough.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#SBATCH --job-name=sage_flythrough
+#SBATCH --output=slurm-flythrough-%A_%a.out
+#SBATCH --array=0-5
+#SBATCH --cpus-per-task=1
+#SBATCH --time=02:00:00
+#SBATCH --mem=2G
+
+# List of argument sets for each array job
+ARGS_LIST=(
+  "orbit density mov"
+  "orbit type mov"
+  "flythrough density mov"
+  "flythrough type mov"
+  "evolution density mov"
+  "evolution type mov"
+)
+
+ml purge
+ml restore basic
+
+ARGS=${ARGS_LIST[$SLURM_ARRAY_TASK_ID]}
+set -- $ARGS
+
+MODE=$1
+
+if [[ $MODE == "evolution" || $MODE == "combined" ]]; then
+    python plotting/flythrough.py --mode $MODE  --color-by $2 --format $3
+else
+    python plotting/flythrough.py --mode $MODE --color-by $2 --format $3
+fi
